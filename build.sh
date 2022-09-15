15e7a7fd35de20e91ef195806f5d8d6f98a78ee6
Kimmo Hernborg <kimmo.hernborg@gmail.com>
2022-09-15 19:59:26 +0200

    Added build script for building and pushing to Docker Hub

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Log oneliner
+log () { LOG_LEVEL=$2; if [ ! $LOG_LEVEL ]; then LOG_LEVEL="info"; fi; echo -e "\033[38;5;131m[${LOG_LEVEL}] \033[38;5;147m$1\033[0m"; }
+
+# Variables
+IMAGE_NAME="kimmohernborg/php-docker"
+DOCKER_CMD="docker"
+if [[ $1 != "" ]]; then EXECUTE=true; else EXECUTE=false; fi
+#echo "EXECUTE = $EXECUTE"
+
+log "Make sure you are logged in to Docker before running this script..."
+echo
+
+for folder in */ ; do
+  TAG=$(echo "$folder" | grep -oE '[0-9.]+')
+  if [[ $TAG == "7.2" || $TAG == "7.3" ]]; then continue; fi # Skip 7.2 and 7.3 (@todo remove from repo)
+
+  pushd "$folder" || exit
+  echo
+  log "Build '$IMAGE_NAME:$TAG'" "build"
+  log "$DOCKER_CMD build --pull -t \"$IMAGE_NAME:$TAG\" ." "cmd"
+  if $EXECUTE; then $DOCKER_CMD build -t "$IMAGE_NAME:$TAG" .; fi
+  echo
+  log "Push '$IMAGE_NAME:$TAG'" "push"
+  log "$DOCKER_CMD push \"$IMAGE_NAME:$TAG\"" "cmd"
+  if $EXECUTE; then $DOCKER_CMD push "$IMAGE_NAME:$TAG"; fi
+  popd || exit
+done
+
+echo
+log "Tag LATEST image and push"
+log "$DOCKER_CMD tag \"$IMAGE_NAME:$TAG\" \"$IMAGE_NAME:latest\"" "cmd"
+if $EXECUTE; then $DOCKER_CMD tag "$IMAGE_NAME:$TAG" "$IMAGE_NAME:latest"; fi
+log "$DOCKER_CMD push \"$IMAGE_NAME:latest\"" "cmd"
+if $EXECUTE; then $DOCKER_CMD push "$IMAGE_NAME:latest"; fi
+echo
+log "Done!"
