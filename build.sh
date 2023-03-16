057c9711b730effe16d8e196b5aebab2177ff8d3
Kimmo Hernborg <kimmo.hernborg@gmail.com>
2023-03-16 13:16:34 +0100

    Added PHP8.2

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ echo
 
 for folder in */ ; do
   TAG=$(echo "$folder" | grep -oE '[0-9.]+')
-  if [[ $TAG == "7.2" || $TAG == "7.3" ]]; then continue; fi # Skip 7.2 and 7.3 (@todo remove from repo)
+  if [[ $TAG == "5.6" || $TAG == "7.2" || $TAG == "7.3" ]]; then continue; fi # Skip 5.6, 7.2 and 7.3 (@todo remove from repo)
 
   pushd "$folder" || exit
   echo
